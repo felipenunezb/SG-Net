1b2ae59ede1b3ec5d8ada4a5106b7378c140a7f1
Felipe Nunez <56325305+felipenunezb@users.noreply.github.com>
2020-10-20 11:33:06 +1100

    Update run_squad2.py

--- run_squad2.py
+++ run_squad2.py
@@ -195,12 +195,12 @@ def read_squad_examples(input_file, input_tag_file, is_training, to_ix_dict):
         tag_rep = tag_data["tag_rep"]
         dqtag_data = {
             "qas_id": qas_id,
-            "head_que": [int(i) for i in tag_rep["pred_head_que"]],
+            #"head_que": [int(i) for i in tag_rep["pred_head_que"]],
             "span_que": [eval(i) for i in tag_rep["hpsg_list_que"]],
-            "type_que": tag_rep["pred_type_que"],
+            #"type_que": tag_rep["pred_type_que"],
             "span_doc": [eval(i) for sen_span in tag_rep["hpsg_list_doc"] for i in sen_span],
-            "type_doc": [i for sen in tag_rep["pred_type_doc"] for i in sen],
-            "head_doc": [int(i) for sen_head in tag_rep["pred_head_doc"] for i in sen_head],
+            #"type_doc": [i for sen in tag_rep["pred_type_doc"] for i in sen],
+            #"head_doc": [int(i) for sen_head in tag_rep["pred_head_doc"] for i in sen_head],
             "token_doc": [token for sen_token in tag_rep['doc_tokens'] for token in sen_token],
             "token_que": tag_rep['que_tokens']
         }
@@ -282,8 +282,8 @@ def read_squad_examples(input_file, input_tag_file, is_training, to_ix_dict):
                 assert dqtag["qas_id"] == qas_id
 
                 span_doc = dqtag["span_doc"]
-                head_doc = dqtag["head_doc"]
-                type_doc = dqtag["type_doc"]
+                #head_doc = dqtag["head_doc"]
+                #type_doc = dqtag["type_doc"]
                 #assert len(span_doc) == len(head_doc) == len(type_doc) == cnt_token, qas_id
                 # reconstruct into sentences
                 new_span_doc = []
@@ -292,16 +292,16 @@ def read_squad_examples(input_file, input_tag_file, is_training, to_ix_dict):
                 cnt = 0
                 for sent_tokens in new_sen_list:
                     new_span_sen = []
-                    new_head_sen = []
-                    new_type_sen = []
+                    #new_head_sen = []
+                    #new_type_sen = []
                     for _ in sent_tokens:
                         new_span_sen.append(span_doc[cnt])
-                        new_head_sen.append(head_doc[cnt])
-                        new_type_sen.append(type_doc[cnt])
+                        #new_head_sen.append(head_doc[cnt])
+                        #new_type_sen.append(type_doc[cnt])
                         cnt += 1
                     new_span_doc.append(new_span_sen)
-                    new_head_doc.append(new_head_sen)
-                    new_type_doc.append(new_type_sen)
+                    #new_head_doc.append(new_head_sen)
+                    #new_type_doc.append(new_type_sen)
 
                 question_text = qa["question"]
                 start_position = None
@@ -347,11 +347,11 @@ def read_squad_examples(input_file, input_tag_file, is_training, to_ix_dict):
                     start_position=start_position,
                     end_position=end_position,
                     is_impossible=is_impossible,
-                    que_heads=dqtag["head_que"],
-                    que_types=dqtag["type_que"],
+                    que_heads=None, #dqtag["head_que"],
+                    que_types=None, #dqtag["type_que"],
                     que_span=dqtag["span_que"],
-                    doc_heads=new_head_doc,
-                    doc_types=new_type_doc,
+                    doc_heads=None, #new_head_doc,
+                    doc_types=None, #new_type_doc,
                     doc_span=new_span_doc,
                     token_doc=dqtag["token_doc"],
                     token_que=dqtag["token_que"],
